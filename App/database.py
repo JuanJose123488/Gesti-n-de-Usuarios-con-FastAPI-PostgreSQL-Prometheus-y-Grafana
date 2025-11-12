919a14bddead59bf9b6a001268725dab7e143c9d
JuanJose123488 <juan_jo.gomez@uao.edu.co>
2025-11-12 17:19:15 -0500

    Primer commit - proyecto FastAPI PostgreSQL Prometheus Grafana

---
+++ App/database.py
@@ -0,0 +1,12 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL environment variable not set")
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
